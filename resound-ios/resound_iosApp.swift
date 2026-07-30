0ed09b10a9996f4c9ed868e5fd8e8807d571e324
Daniel <danielrobertson733@gmail.com>
2026-07-30 18:09:54 -0500

    Initial Commit

---
+++ resound-ios/resound_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  resound_iosApp.swift
+//  resound-ios
+//
+//  Created by Daniel Robertson on 7/30/26.
+//
+
+import SwiftUI
+
+@main
+struct resound_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
